40a1a5cdb54b28a2cef72c36e457e15becf9a846
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-05-15 00:57:17 -0300

    adicionando classe escolha aleatoria embaralhador

--- src/main/java/Embaralhador.java
+++ src/main/java/Embaralhador.java
@@ -1,5 +1,4 @@
 public interface Embaralhador {
 
     String embaralhar(String palavra);
-
 }
--- src/main/java/FabricaEmbaralhadores.java
+++ src/main/java/FabricaEmbaralhadores.java
@@ -1,3 +1,19 @@
+import java.util.Random;
+
 public class FabricaEmbaralhadores {
 
+    public static final Random RANDOM = new Random();
+
+    public Embaralhador getEmbaralhadorAleatorio(){
+        int escolha = RANDOM.nextInt(2);
+
+        switch(escolha){
+            case 0:
+                return new InverterString();
+            case 1:
+                return new EmbaralharLetras();
+            default:
+                throw new IllegalStateException("Embaralhador não encontrado");
+        }
+    }
 }
